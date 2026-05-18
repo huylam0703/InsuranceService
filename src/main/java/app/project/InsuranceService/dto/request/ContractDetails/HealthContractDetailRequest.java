69569abfbc9c8ad80e2d6bbdd4223b48f7968a8d
huylam0703 <huylamnhat244@gmail.com>
2026-05-18 21:39:17 +0700

    refactor: add validation for requets contract detail, policy

--- src/main/java/app/project/InsuranceService/dto/request/ContractDetails/HealthContractDetailRequest.java
+++ src/main/java/app/project/InsuranceService/dto/request/ContractDetails/HealthContractDetailRequest.java
@@ -1,5 +1,6 @@
 package app.project.InsuranceService.dto.request.ContractDetails;
 
+import jakarta.validation.constraints.*;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
 
@@ -10,17 +11,31 @@ import lombok.experimental.FieldDefaults;
 @Builder
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class HealthContractDetailRequest {
+
+    @NotBlank(message = "HEALTH_HEIGHT_CONTRACT")
+    @DecimalMin(value = "50.0", message = "MIN_HEIGHT_HEALTH")
+    @DecimalMax(value = "300.0", message = "MAX_HEIGHT_HEALTH")
     Double heightCm;
 
+    @NotBlank(message = "HEALTH_WEIGHT_CONTRACT")
     Double weightKg;
 
+    @NotBlank(message = "BLOOD_TYPE_HEALTH")
+    @Pattern(
+            regexp = "^(A|B|AB|O)[+-]$",
+            message = "BLOOD_TYPE"
+    )
     String bloodType;
 
+    @Size(max = 1000, message = "MEDICAL_HISTORY_HEALTH")
     String medicalHistory;
 
+    @Size(max = 500, message = "ALLERGIES_HEALTH")
     String allergies;
 
+    @NotNull(message = "SMOKING_HEALTH")
     Boolean smoking;
 
+    @NotNull(message = "ALCOHOL_USE_HEALTH")
     Boolean alcoholUse;
 }
--- src/main/java/app/project/InsuranceService/dto/request/ContractDetails/LifeContractDetailRequest.java
+++ src/main/java/app/project/InsuranceService/dto/request/ContractDetails/LifeContractDetailRequest.java
@@ -1,5 +1,6 @@
 package app.project.InsuranceService.dto.request.ContractDetails;
 
+import jakarta.validation.constraints.*;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
 
@@ -12,17 +13,35 @@ import java.math.BigDecimal;
 @Builder
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class LifeContractDetailRequest {
+
+    @NotBlank(message = "OCCUPATION_LIFE")
     String occupation;
 
+    @NotNull(message = "ANNUAL_INCOME_LIFE")
+    @DecimalMin(value = "0.0", inclusive = false,
+            message = "ANNUAL_INCOME")
+    @Digits(integer = 15, fraction = 2,
+            message = "ANNUAL_INCOME_INVALID")
     BigDecimal annualIncome;
 
+    @NotBlank(message = "BENEFICIARY_NAME_LIFE")
     String beneficiaryName;
 
+    @NotBlank(message = "BENEFICIARY_RELATIONSHIP_LIFE")
     String beneficiaryRelationship;
 
+    @NotBlank(message = "BENEFICIARY_PHONE_LIFE")
+    @Pattern(
+            regexp = "^(0|\\+84)[0-9]{9,10}$",
+            message = "BENEFICIARY_PHONE_INVALID"
+    )
     String beneficiaryPhone;
+
+    @Size(max = 1000,
+            message = "MEDICAL_HISTORY_LIFE")
 
     String medicalHistory;
 
+    @NotNull(message = "SMOKING_LIFE")
     Boolean smoking;
 }
